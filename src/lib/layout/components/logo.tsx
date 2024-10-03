4b10d63bc358cd2f486826390dacfddc5af1abac
hulxv <hulxxv@gmail.com>
2024-10-04 00:05:08 +0300

    feat: improve logo component

--- src/lib/layout/components/logo.tsx
+++ src/lib/layout/components/logo.tsx
@@ -1,13 +1,13 @@
 import { Image } from "@chakra-ui/react"
 
-export function Logo() {
+export function Logo({ isCropped, width = 50 }: { isCropped?: boolean, width?: number }) {
     return (
         <Image
-            src={LOGO_SRC}
+            src={isCropped ? LOGO_CROPPED_SRC : LOGO_SRC}
             borderRadius={8}
-            boxSize={50}
+            width={width}
         />
     )
 }
-
+export const LOGO_CROPPED_SRC = "/assets/logo-cropped.png"
 export const LOGO_SRC = "/assets/crop-compass-logo.webp";
