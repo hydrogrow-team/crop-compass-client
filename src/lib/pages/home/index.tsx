dbbcda95a9593004f99d5ef7264b99604dae69f1
hulxv <hulxxv@gmail.com>
2024-10-04 01:28:33 +0300

    feat: landing page

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,10 +1,33 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, Image, HStack, Highlight, Stack, Button } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 
 
 const Home = () => {
+  const navigate = useNavigate()
   return (
-    <Heading size="md">Home Page</Heading>
+
+    <HStack width={"full"} height={"full"} justifyItems={"center"} alignItems={"center"} spacing={10}>
+      <Stack>
+
+        <Heading lineHeight='tall'>
+          <Highlight
+            query={['future']}
+            styles={{ px: '2', py: '1', rounded: '5px', bg: 'green.100', textTransform: "uppercase" }}
+          >
+            Your future is coming!
+          </Highlight>
+          <br />
+          Make your farm land ready for increase its productivity!
+        </Heading>
+
+        <Button colorScheme='green' size='lg' onClick={() => navigate("/auth/signup")}>
+          Getting Started now!
+        </Button>
+      </Stack>
+
+      <Image width={"40%"} src="/assets/wall.jpg" />
+    </HStack>
   );
 };
 
